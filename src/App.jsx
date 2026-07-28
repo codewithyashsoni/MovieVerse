9bd0ba861c4cd3cfc4c311fcf4b5a4c6287e09e9
codewithyashsoni <yashsoni.contact@gmail.com>
2026-07-28 14:58:06 +0530

    Added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
+import Home from "./pages/Home.jsx"
+import NavBar from "./components/NavBar.jsx"
 
 function App() {
+  const [phase, setPhase] = useState("home");
 
   return (
     <>
-      
-    </>
+    <NavBar setPhase={setPhase} />
+
+    <div className="container">
+      {phase === "home" && <Home/>}
+    </div></>
+    
   )
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,33 @@
+import React, {useState} from "react"
+import {Clapperboard} from "lucide-react"
+
+function NavBar({setPhase}){
+    return(
+        <div className="navbar-wrapper">
+            <nav className="navbar">
+                <div className="logo">
+                    <Clapperboard className="logo-icon" />
+                    <span>MovieVerse</span>
+                </div>
+
+                <ul className="nav-list">
+                    <li className="nav-list-item">
+                        <button 
+                        className="nav-button"
+                        onClick={() => setPhase("home")}
+                        >Home</button>
+                    </li>
+                    <li className="nav-list-item">
+                        <button 
+                        className="nav-button"
+                        onClick={() => setPhase("favourite")}
+                        >Favourites</button>
+                    </li>
+                </ul>
+
+            </nav>
+        </div>
+        
+    )
+}
+export default NavBar
